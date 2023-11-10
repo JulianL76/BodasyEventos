d1c42fb4f0aa96667759f568545f89642ebfd9d0
JulianL76 <crisjulian_07@outlook.com>
2023-11-10 18:27:04 -0500

    Ajustes Varios del  Index

--- src/main/java/com/app/web/controller/TrajeController.java
+++ src/main/java/com/app/web/controller/TrajeController.java
@@ -2,7 +2,7 @@ package com.app.web.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.apache.commons.codec.binary.Base64;
+//import org.apache.commons.codec.binary.Base64;
 
 import java.io.IOException;
 import java.util.List;
@@ -21,16 +21,16 @@ import com.app.web.entities.Traje;
 import com.app.web.repository.CategoriaRepository;
 import com.app.web.services.TrajeService;
 
-
 @Controller
 public class TrajeController {
     private final CategoriaRepository categoriaRepository;
     @Autowired
     private TrajeService trajeService;
-    
+
     public TrajeController(CategoriaRepository categoriaRepository) {
         this.categoriaRepository = categoriaRepository;
     }
+
     @GetMapping("/inicio/registroTraje")
     public String listarTrajes(Model model) {
         model.addAttribute("trajes", trajeService.obtenerTodosTrajes());
@@ -40,7 +40,7 @@ public class TrajeController {
     @GetMapping("/inicio/registroTraje/nuevo")
     public String mostrarFormularioNuevo(Model model) {
         List<Categoria> categorias = categoriaRepository.findAll();
-    
+
         if (!categorias.isEmpty()) {
             model.addAttribute("categorias", categorias);
             model.addAttribute("traje", new Traje());
@@ -52,25 +52,52 @@ public class TrajeController {
         }
     }
 
-    @PostMapping("/inicio/registroTraje/nuevo")
+    // @PostMapping("/inicio/registroTraje/nuevo")
+    // public String guardarTraje(@ModelAttribute Traje traje, @RequestParam("file")
+    // MultipartFile file) {
+    // // Verificar que la imagen se haya cargado correctamente y no esté vacía.
+    // if (!file.isEmpty()) {
+    // try {
+    // byte[] bytes = file.getBytes();
+    // // Convertir la imagen a formato Base64 utilizando Apache Commons Codec.
+    // String base64Image = Base64.encodeBase64String(bytes);
+
+    // // Validar la conversión a Base64 antes de guardarla en la entidad.
+    // if (base64Image != null && !base64Image.isEmpty()) {
+    // traje.setImagenes(base64Image.getBytes());
+    // trajeService.guardarTraje(traje);
+    // return "redirect:/inicio/registroTraje";
+    // } else {
+    // // Manejo de errores si la conversión no fue exitosa.
+    // // Puedes redirigir a una página de error o realizar alguna otra acción.
+    // return "errorPage"; // Reemplaza "errorPage" con la vista de error de tu
+    // elección.
+    // }
+    // } catch (IOException e) {
+    // e.printStackTrace();
+    // // Manejo de errores si hay un problema al obtener los bytes de la imagen.
+    // // Puedes redirigir a una página de error o realizar alguna otra acción.
+    // return "errorPage"; // Reemplaza "errorPage" con la vista de error de tu
+    // elección.
+    // }
+    // } else {
+    // // Manejo de errores si la imagen está vacía.
+    // // Puedes redirigir a una página de error o realizar alguna otra acción.
+    // return "errorPage"; // Reemplaza "errorPage" con la vista de error de tu
+    // elección.
+    // }
+    // }
+
     public String guardarTraje(@ModelAttribute Traje traje, @RequestParam("file") MultipartFile file) {
         // Verificar que la imagen se haya cargado correctamente y no esté vacía.
         if (!file.isEmpty()) {
             try {
-                byte[] bytes = file.getBytes();
-                // Convertir la imagen a formato Base64 utilizando Apache Commons Codec.
-                String base64Image = Base64.encodeBase64String(bytes);
-                
-                // Validar la conversión a Base64 antes de guardarla en la entidad.
-                if (base64Image != null && !base64Image.isEmpty()) {
-                    traje.setImagenes(base64Image.getBytes());
-                    trajeService.guardarTraje(traje);
-                    return "redirect:/inicio/registroTraje";
-                } else {
-                    // Manejo de errores si la conversión no fue exitosa.
-                    // Puedes redirigir a una página de error o realizar alguna otra acción.
-                    return "errorPage"; // Reemplaza "errorPage" con la vista de error de tu elección.
+                if (!file.isEmpty()) {
+                    byte[] bytes = file.getBytes();
+                    traje.setImagenes(bytes);
                 }
+                trajeService.guardarTraje(traje);
+                return "redirect:/inicio/registroTraje";
             } catch (IOException e) {
                 e.printStackTrace();
                 // Manejo de errores si hay un problema al obtener los bytes de la imagen.
@@ -83,6 +110,7 @@ public class TrajeController {
             return "errorPage"; // Reemplaza "errorPage" con la vista de error de tu elección.
         }
     }
+
     @GetMapping("/editar/{id}")
     public String mostrarFormularioEditar(@PathVariable Integer id, Model model) {
         Traje traje = trajeService.obtenerTrajePorId(id);
@@ -91,7 +119,8 @@ public class TrajeController {
     }
 
     @PostMapping("/editar/{id}")
-    public String actualizarTraje(@PathVariable Integer id, @ModelAttribute Traje traje, @RequestParam("file") MultipartFile file) {
+    public String actualizarTraje(@PathVariable Integer id, @ModelAttribute Traje traje,
+            @RequestParam("file") MultipartFile file) {
         if (!file.isEmpty()) {
             try {
                 byte[] imagenes = file.getBytes();
@@ -110,21 +139,22 @@ public class TrajeController {
         return "redirect:/inicio/registroTraje";
     }
 
-
-/*     @GetMapping("/inicio/registroTraje/ver-imagen/{idTraje}")
-    public String verImagen(@PathVariable Integer idTraje, Model model) {
-    Traje traje = trajeService.obtenerTrajePorId(idTraje);
-    if (traje != null && traje.getImagenes() != null) {
-        // Convierte el arreglo de bytes en una representación en Base64
-        String base64Image = Base64.getEncoder().encodeToString(traje.getImagenes());
-
-        // Agrega la imagen a la vista
-        model.addAttribute("imagenBase64", base64Image);
-
-        return "verImagen"; // Crea una nueva plantilla para mostrar la imagen
-    } else {
-        // Manejo de errores si el traje o la imagen no existen
-        return "error"; // Crea una vista de error
-    }
-} */
+    /*
+     * @GetMapping("/inicio/registroTraje/ver-imagen/{idTraje}")
+     * public String verImagen(@PathVariable Integer idTraje, Model model) {
+     * Traje traje = trajeService.obtenerTrajePorId(idTraje);
+     * if (traje != null && traje.getImagenes() != null) {
+     * // Convierte el arreglo de bytes en una representación en Base64
+     * String base64Image = Base64.getEncoder().encodeToString(traje.getImagenes());
+     * 
+     * // Agrega la imagen a la vista
+     * model.addAttribute("imagenBase64", base64Image);
+     * 
+     * return "verImagen"; // Crea una nueva plantilla para mostrar la imagen
+     * } else {
+     * // Manejo de errores si el traje o la imagen no existen
+     * return "error"; // Crea una vista de error
+     * }
+     * }
+     */
 }
