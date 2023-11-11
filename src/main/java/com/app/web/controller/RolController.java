f5b65becf3b1b57a567d5fcd0af76cfa04795837
JulianL76 <crisjulian_07@outlook.com>
2023-11-11 08:15:47 -0500

    Creacion de varias Pantallas y Ajustes CSS

--- src/main/java/com/app/web/controller/RolController.java
+++ src/main/java/com/app/web/controller/RolController.java
@@ -2,7 +2,6 @@ package com.app.web.controller;
 
 import java.util.List;
 import java.util.Map;
-// import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -10,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-// import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -19,8 +17,6 @@ import com.app.web.entities.Rol;
 import com.app.web.entities.Usuario;
 import com.app.web.repository.PermisoRepository;
 import com.app.web.repository.RolRepository;
-// import com.app.web.repository.UsuarioRepository;
-import com.app.web.services.UsuarioService;
 
 @Controller
 public class RolController {
@@ -30,12 +26,6 @@ public class RolController {
 	@Autowired
 	private PermisoRepository permisoRepository;
 
-	@Autowired
-	private UsuarioService usuarioService;
-
-	@Autowired
-	// private UsuarioRepository usuarioRepository;
-
 	@GetMapping("/inicio/configuracion")
 	public String panelDeConfig(Model model) {
 		Usuario usuario = new Usuario();
@@ -44,29 +34,32 @@ public class RolController {
 		model.addAttribute("permisos", permisos);
 		List<Rol> roles = rolRepository.findAll();
 		model.addAttribute("roles", roles);
-		List<Usuario> usuarios = usuarioService.listarUsuarios();
-		model.addAttribute("usuarios", usuarios);
 		return "configuracion";
 	}
 
 	@PostMapping("/inicio/configuracion")
 	public String guardarRol(@RequestParam Map<String, String> params, @RequestParam String nombre, Model model) {
-		model.addAttribute("usuario", new Usuario());
-		Set<Integer> permisosActivados = params.keySet().stream().filter(key -> key.startsWith("permiso_"))
-				.map(key -> Integer.parseInt(key.replace("permiso_", ""))).collect(Collectors.toSet());
+		Set<Integer> permisosActivados = params.keySet().stream()
+				.filter(key -> key.startsWith("permiso_"))
+				.map(key -> Integer.parseInt(key.replace("permiso_", "")))
+				.collect(Collectors.toSet());
 		List<Rol> rolesExistentes = rolRepository.findAll();
 		// Variable para almacenar el rol existente con los mismos permisos
 		Rol rolExistenteConPermisos = null;
 		// Verificar si ya existe un rol con los mismos permisos
 		for (Rol rol : rolesExistentes) {
-			if (rol.getPermisos().stream().map(Permiso::getId).collect(Collectors.toSet()).equals(permisosActivados)) {
+			if (rol.getPermisos().stream()
+					.map(Permiso::getId)
+					.collect(Collectors.toSet())
+					.equals(permisosActivados)) {
 				rolExistenteConPermisos = rol;
 				break; // Rompe el bucle una vez que se encuentra un rol con los mismos permisos
 			}
 		}
 		if (rolExistenteConPermisos != null) {
 			model.addAttribute("error", "El rol " + rolExistenteConPermisos.getNombre() + " ya tiene estos permisos.");
-			panelDeConfig(model);
+			List<Permiso> permisos = permisoRepository.findAll();
+			model.addAttribute("permisos", permisos);
 			return "configuracion";
 		}
 		// Si no existe un rol con los mismos permisos, crea un nuevo rol
@@ -75,7 +68,6 @@ public class RolController {
 		Set<Permiso> permisosNuevos = permisoRepository.getPermisosByIds(permisosActivados);
 		rol.setPermisos(permisosNuevos);
 		rolRepository.save(rol);
-		return "redirect:/inicio/configuracion?exito";
+		return "redirect:/inicio/configuracion";
 	}
-
 }
